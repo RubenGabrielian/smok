1e5bd8665b1df73693b2b5ac4a4cfc4d301dbcaf
Ruben.Gabrielyan <gabrielianruben@gmail.com>
2025-10-01 16:43:26 +0400

    log

--- src/components/MotivationCard/MotivationCard.jsx
+++ src/components/MotivationCard/MotivationCard.jsx
@@ -3,12 +3,8 @@ import './MotivationCard.css'
 const MotivationCard = () => {
     return (
         <div className="motivation-card">
-            <div className="motivation-icon">🚭</div>
-            <h2 className="motivation-title">Together We Quit Smoking</h2>
-            <p className="motivation-text">
-                You&apos;re not alone on this journey. We&apos;re here to support you every step of the way.
-                Every cigarette you don&apos;t smoke is a victory! 💪
-            </p>
+            <span className="motivation-icon">🚭</span>
+            <span className="motivation-text">You&apos;re stronger than this habit!</span>
         </div>
     )
 }
